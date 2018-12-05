0be1d5cb52a77293ea0b7941325cd05efd5533e3
RyzeZhao <643080112@qq.com>
2018-12-05 17:36:19 +0800

    DesignPattern：外观模式（Facade Pattern）,水果

---
+++ facade-pattern/src/main/java/com/zhaolearn/facade/Watermalon.java
@@ -0,0 +1,8 @@
+package com.zhaolearn.facade;
+
+public class Watermalon  implements Fruit{
+    @Override
+    public void tellMeName() {
+        System.out.println("My Name is Watermalon");
+    }
+}
