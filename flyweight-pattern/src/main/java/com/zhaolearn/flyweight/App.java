028ec7cf7e3a0a7509860b760df58866c375c9d4
RyzeZhao <643080112@qq.com>
2018-12-05 16:50:10 +0800

    DesignPattern：享元模式（Flyweight Pattern）

---
+++ flyweight-pattern/src/main/java/com/zhaolearn/flyweight/App.java
@@ -0,0 +1,13 @@
+package com.zhaolearn.flyweight;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
