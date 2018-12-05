6c4df901b0097c75fecbfa0fb718b4634666ad0e
RyzeZhao <643080112@qq.com>
2018-12-05 17:36:45 +0800

    DesignPattern：外观模式（Facade Pattern），外观

---
+++ facade-pattern/src/main/java/com/zhaolearn/facade/FruitMaker.java
@@ -0,0 +1,23 @@
+package com.zhaolearn.facade;
+
+public class FruitMaker {
+    private Fruit apple;
+    private Fruit orange;
+    private Fruit watermalon;
+
+    public FruitMaker() {
+        apple = new Apple();
+        orange = new Orange();
+        watermalon = new Watermalon();
+    }
+
+    public void tellMeNameApple(){
+        apple.tellMeName();
+    }
+    public void tellMeNameOrange(){
+        orange.tellMeName();
+    }
+    public void tellMeNameWatermalon(){
+        watermalon.tellMeName();
+    }
+}
