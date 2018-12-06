f37ceab3a3190896d87630145ec1d74fb20492a1
RyzeZhao <643080112@qq.com>
2018-12-06 14:22:07 +0800

    DesignPattern：责任链模式（Chain of Responsibility Pattern）

---
+++ chain-of-esponsibility-pattern/src/main/java/com/zhaolearn/chainofesponsibility/App.java
@@ -0,0 +1,13 @@
+package com.zhaolearn.chainofesponsibility;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
