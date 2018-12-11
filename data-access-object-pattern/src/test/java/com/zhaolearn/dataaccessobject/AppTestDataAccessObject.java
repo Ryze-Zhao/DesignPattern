343d491b880dd520c9b73d6629a70364c77ffa65
RyzeZhao <643080112@qq.com>
2018-12-11 15:19:01 +0800

    DesignPattern：数据访问对象模式（Data Access Object Pattern）

---
+++ data-access-object-pattern/src/test/java/com/zhaolearn/dataaccessobject/AppTestDataAccessObject.java
@@ -0,0 +1,27 @@
+package com.zhaolearn.dataaccessobject;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTestDataAccessObject {
+    @Test
+    public void appTestDataAccessObject() {
+        StudentDao studentDao = new StudentDaoImpl();
+        //输出所有的学生
+        for (Student student : studentDao.getAllStudents()) {
+            System.out.println("Student: [RollNo : " + student.getRollNo() + ", Name : " + student.getName() + " ]");
+        }
+        //更新学生
+        Student student = studentDao.getAllStudents().get(0);
+        student.setName("Michael");
+        studentDao.updateStudent(student);
+        //获取学生
+        studentDao.getStudent(0);
+        System.out.println("Student: [RollNo : " + student.getRollNo() + ", Name : " + student.getName() + " ]");
+    }
+
+}
