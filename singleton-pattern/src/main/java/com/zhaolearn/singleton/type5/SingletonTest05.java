136c99e48a63745a51bb0615a3313c7fa19b5df6
RyzeZhao <643080112@qq.com>
2020-01-10 15:33:35 +0800

    DesignPattern：单例模式（Singleton Pattern）：双重检查

---
+++ singleton-pattern/src/main/java/com/zhaolearn/singleton/type5/SingletonTest05.java
@@ -0,0 +1,36 @@
+package com.zhaolearn.singleton.type5;
+
+/**
+ * 双重检查
+ *
+ * @author: HeHaoZhao
+ * @date: 2020/1/10 15:18
+ */
+public class SingletonTest05 {
+	public static void main(String[] args) {
+		System.out.println("双重检查");
+		Singleton instance = Singleton.getInstance();
+		Singleton instance2 = Singleton.getInstance();
+		System.out.println(instance == instance2); // true
+		System.out.println("instance.hashCode=" + instance.hashCode());
+		System.out.println("instance2.hashCode=" + instance2.hashCode());
+	}
+}
+
+// 双重检查
+class Singleton {
+	private static volatile Singleton instance;
+	private Singleton() {}
+	//提供一个静态的公有方法，加入双重检查代码，解决线程安全问题, 同时解决懒加载问题
+	//同时保证了效率, 推荐使用
+	public static synchronized Singleton getInstance() {
+		if(instance == null) {
+			synchronized (Singleton.class) {
+				if(instance == null) {
+					instance = new Singleton();
+				}
+			}
+		}
+		return instance;
+	}
+}
