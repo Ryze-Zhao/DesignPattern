f9116fc799cc181554bc5c33e93012eeba03c3ee
RyzeZhao <643080112@qq.com>
2020-02-11 21:55:46 +0800

    DesignPattern：解释器模式（Interpreter Pattern）：解释器模式在Spring框架应用的源码剖析

---
+++ interpreter-pattern/src/main/java/com/zhaolearn/interpreter/example/SpringTest.java
@@ -0,0 +1,15 @@
+package com.zhaolearn.interpreter.example;
+/**
+ * 解释器模式在Spring框架应用的源码剖析
+ *
+ * @author: HeHaoZhao
+ * @date: 2020/2/11 21:55
+ */
+public class SpringTest {
+    public static void main(String[] args) {
+        //代码需要引入Spring才可以正常使用
+//		SpelExpressionParser parser=new SpelExpressionParser();
+//		Expression expression =parser. parseExpression("10*(2+1)*1+66");
+//		int result=(Integer) expression. getValue(); System. out. println(result);
+    }
+}
