3090048683ff83311d61fc504cfcd1a23a80b355
RyzeZhao <643080112@qq.com>
2020-02-11 12:31:15 +0800

    DesignPattern：备忘录模式（Memento Pattern）：迁移旧代码

--- memento-pattern/src/test/java/com/zhaolearn/memento/AppTestMemento.java
+++ memento-pattern/src/test/java/com/zhaolearn/memento/AppTestMemento.java
@@ -11,19 +11,6 @@ public class AppTestMemento {
 
     @Test
     public void appTestMemento() {
-        Originator originator = new Originator();
-        CareTaker careTaker = new CareTaker();
-        originator.setStatus("状态1");
-        originator.setStatus("状态2");
-        careTaker.add(originator.saveStatusToMemento());
-        originator.setStatus("状态3");
-        careTaker.add(originator.saveStatusToMemento());
-        originator.setStatus("状态4");
-        System.out.println("当前状态: " + originator.getStatus());
-        originator.getStatusFromMemento(careTaker.get(0));
-        System.out.println("第一次保存状态: " + originator.getStatus());
-        originator.getStatusFromMemento(careTaker.get(1));
-        System.out.println("第二次保存状态: " + originator.getStatus());
 
     }
 }
